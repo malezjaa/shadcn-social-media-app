4400c69839f0b1ce6f29623645a45aa7b777c96a
BuildTools <poreba.kris@gmail.com>
2023-06-18 19:51:20 +0200

    commit

--- lib/api/axios.ts
+++ lib/api/axios.ts
@@ -1,9 +1,9 @@
 import axios, { AxiosRequestConfig } from "axios";
-import {getBaseUrl} from "@/lib/urls";
+import { getBaseUrl } from "@/lib/urls";
 
 export const axiosClient = axios.create({
   maxBodyLength: Infinity,
-  baseURL: getBaseUrl() + "/api",
+  baseURL: "https://shadcn-social-media-app.vercel.app/api",
 });
 
 export const config: AxiosRequestConfig = {
